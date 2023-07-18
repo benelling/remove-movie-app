778d882250e65dfdf187583e7b1ed3f0891ad23c
benelling <smbt246@naver.com>
2023-07-18 09:49:02 +0900

    Create project

---
+++ src/components/TheFooter.js
@@ -0,0 +1,24 @@
+import { Component } from "../core/benelling";
+import aboutStore from "../store/about"
+
+export default class TheFooter extends Component {
+  constructor() {
+    super({
+      tagName: 'footer'
+    })
+  }
+  render() {
+    const { github, repository } = aboutStore.state
+    this.el.innerHTML = /* html */ `
+      <div>
+        <a href="${repository}">Github Repository</a>
+      </div>
+      <div>
+        <a href="${github}">
+          ${new Date().getFullYear()}
+          BENELLING
+        </a>
+      </div>
+    `
+  }
+}
---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from "../core/benelling";
+
+export default new Store({
+  photo: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_1280.png',
+  name: 'BENELLING / Mo HyeIn',
+  email: 'smbt246@naver.com',
+  blog: 'https://blog.naver.com/smbt246',
+  github: 'https://github.com/benelling',
+  repository: 'https://github.com/benelling/vanillajs-movie-app'
+})
